6dc973bc17521eb0960ce75f77d5905d8fc2dd05
JJ <73240114+JJudge0@users.noreply.github.com>
2021-10-22 11:18:55 +0100

    Lab1-HelloWorld

---
+++ Lab1-HelloWorld.vb
@@ -0,0 +1,9 @@
+Module Module1
+    Sub Main()
+        'Name: Jatinder Judge StudentID:1945979
+        Console.WriteLine("Hello world") 'writes it on a new line
+        Console.Write("Goodbye world") 'writes it on the same line
+        ' This is just the testing phase of how to write in the console command line
+    End Sub
+
+End Module
